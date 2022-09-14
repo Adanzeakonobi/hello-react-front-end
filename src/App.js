0c4fb4d776e65df347235f380bd20b6f57ebda9a
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-09-14 11:28:58 -0600

    Create react app and install packages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
